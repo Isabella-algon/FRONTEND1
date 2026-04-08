4e4f3c8d6b75dd6ca71371f22a55f0fa45115a9c
Isabella <alvarez.isabella@uces.edu.co>
2026-04-08 09:22:09 -0500

    Subo cambios

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '', 
+        pathMatch: 'full', 
+        redirectTo: '/login',
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/login/login').then((m) => m.Login)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home)
+    },
+];
---
+++ src/app/features/login/login.ts
@@ -0,0 +1,49 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule], // ReactiveFormsModule es clave para lo avanzado
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  private fb = inject(FormBuilder);
+
+  // Usamos SIGNAL para el interruptor (Nivel Avanzado)
+  esRegistro = signal<boolean>(false);
+
+  // Creamos el formulario reactivo con todos tus campos de Optometría
+  form = this.fb.group({
+    full_name: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    email: [''],
+    telephone: [''],
+    identification: ['']
+  });
+
+  // Función para cambiar de modo usando .set() de los Signals
+  toggleModo() {
+    this.esRegistro.set(!this.esRegistro());
+    this.form.reset(); // Limpia los campos al cambiar
+  }
+
+  // Función de ingreso
+  login() {
+    if (this.form.valid) {
+      const datos = this.form.value;
+      alert(`Iniciando sesión como: ${datos.full_name}`);
+    } else {
+      alert('Por favor, llena los campos obligatorios.');
+    }
+  }
+
+  // Función de registro
+  registrar() {
+    const datos = this.form.value;
+    alert(`Registro de Optometría Exitoso:\nUsuario: ${datos.full_name}\nDocumento: ${datos.identification}`);
+    this.esRegistro.set(false); // Vuelve al login
+  }
+}
